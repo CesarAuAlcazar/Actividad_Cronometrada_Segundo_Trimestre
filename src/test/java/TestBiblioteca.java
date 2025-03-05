e467265322f17c4bddae19a462aaa37423c7e151
Цезарь А. Альказар Ромеро <cesar.alcazar606@comunidadunir.net>
2025-03-05 11:19:09 +0100

    Update: pequeña actualización del test

--- src/test/java/TestBiblioteca.java
+++ src/test/java/TestBiblioteca.java
@@ -35,19 +35,19 @@ class TestBiblioteca {
     @Test
     void encuentraLibroPorTitulo() {
         biblioteca.agregarLibro(this.libro); // Agregamos un libro
-        Libro libroEncontrado = biblioteca.encuentraLibroPorTitulo("Ruslan y Ludmila"); // Buscamos un libro concreto
-        assertNotNull(libroEncontrado); // Verificamos que se ha encontrado el libro
-        assertEquals(this.libro.getTitulo(), libroEncontrado); //Nos aseguramos que es el libro que buscamos
+        Libro librook = biblioteca.encuentraLibroPorTitulo("Ruslan y Ludmila"); // Buscamos un libro concreto
+        assertNotNull(librook); // Verificamos que se ha encontrado el libro
+        assertEquals(this.libro, librook); //Nos aseguramos que es el libro que buscamos
 
-        Libro libroNoEncontrado = biblioteca.encuentraLibroPorTitulo("Libro Inexistente");
-        assertNull(libroNoEncontrado); // Verifica que el libro no se ha encontrado
+        Libro libroNo = biblioteca.encuentraLibroPorTitulo("Este libro no está en la biblioteca");
+        assertNull(libroNo); // Verifica que el libro no se ha encontrado
     }
 
 
     @Test
     void encuentraLibrosPorAutor() {
         biblioteca.agregarLibro(this.libro);
-        List<Libro> libro1 = biblioteca.encuentraLibrosPorAutor("Donald Knuth"); // Buscamos un autor
+        List<Libro> libro1 = biblioteca.encuentraLibrosPorAutor("Alexander Pushkin"); // Buscamos un autor
         assertNotNull(libro1); // Verificamos que la lista no sea nula
         assertEquals(1, libro1.size()); // Verificamos que haya un libro en la lista
         assertTrue(libro1.contains(this.libro)); // Verificamos que el libro está en la lista
